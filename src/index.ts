6516e94eb3c4083f603bb1e193cd0c1bfbf04f45
Tay Kai Quan <kaitay303@gmail.com>
2022-03-09 06:18:36 +0000

    flags for posBang and multi bang

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ function encodeChars(text: string) {
 
 function openInBackground(url: string) {
 	// HELP!!!!! Please find a way to open URLs in the background.
-	// for some reason it says window isn't defined and its driving me crazy.
+	// for some reason it says window, open, etc. isn't defined and its driving me crazy.
 
-	// window.open(url);
+	// window.open(url, "_new")
 	// window.focus()
+	return
 }
 
 function handleRequest(request: Request) {
@@ -18,32 +19,40 @@ function handleRequest(request: Request) {
 	const query = url.searchParams.get('q') ?? '';
 	const querySplit = query.split(" ")
 	const engine = (url.searchParams.get('engine') ?? SITES.google) as string;
+	const posBang = (url.searchParams.get('posBang') ?? "true") as string;
 
-	// get array of bangs
-	var queryWords = ""
-	var queryBangs = []
+	// check for positional bang = false
+	var mainBang = ""
+	if (posBang == 'true') {
+		// get array of bangs
+		var queryWords = ""
+		var queryBangs = []
 
-	for (const element of querySplit) {
-		if (element.startsWith("!")) {
-			queryBangs.push(element.substring(1, element.length))
-		} else {
-			// if the word is not a bang, add it to the words.
-			queryWords = queryWords.concat(element).concat(" ")
+		for (const element of querySplit) {
+			if (element.startsWith("!")) {
+				queryBangs.push(element.substring(1, element.length))
+			} else {
+				// if the word is not a bang, add it to the words.
+				queryWords = queryWords.concat(element).concat(" ")
+			}
 		}
-	}
 
-	queryWords = queryWords.substring(0, queryWords.length - 1) // remove last character, a space.
+		queryWords = queryWords.substring(0, queryWords.length - 1) // remove last character, a space.
 
-	var mainBang = (queryBangs.shift() ?? '');
-	mainBang = mainBang.toLowerCase().replace('!', '')
-	// if there are multiple bangs, the first one will be taken as "main"
+		mainBang = (queryBangs.shift() ?? '').toLowerCase().replace('!', '')
+		// if there are multiple bangs, the first one will be taken as "main"
 
-	// open additional bangs in new tabs
-	for (const bang of queryBangs) {
-		const site = SITES[bang.toLowerCase()];
-		if (site) {
-			const parsed = typeof site === 'function' ? site(queryWords) : site;
-			openInBackground(parsed.replace('{q}', encodeChars(queryWords)));
+		// open additional bangs in new tabs
+		for (const bang of queryBangs) {
+			const site = SITES[bang.toLowerCase()];
+			if (site) {
+				const parsed = typeof site === 'function' ? site(queryWords) : site;
+				openInBackground(parsed.replace('{q}', encodeChars(queryWords)));
+			}
+		}
+	} else {
+		if (query.startsWith('!')) {
+			mainBang = querySplit[0].toLowerCase().replace('!', '')
 		}
 	}
 
